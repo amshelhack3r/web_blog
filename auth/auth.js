c765a9fa32865e11c1af4c31e11f5adfe65f385a
amshelhack3r <amshelhack3r@gmail.com>
2018-09-30 20:07:27 +0300

    method for checking if a user is authenticated

---
+++ auth/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    checklogin: (req, res, next) => {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+
+        req.flash('error_msg', 'Not Authorized');
+        res.redirect('/users/login');
+    }
+}
