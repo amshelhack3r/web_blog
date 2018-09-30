91e6faf4b4fb3dd1f50374ba84c89682b0fa0b0a
amshelhack3r <amshelhack3r@gmail.com>
2018-09-30 20:10:27 +0300

    add logout function

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 //bring in the model to hash the password
 const User = require("../models/User");
+const passport = require('passport');
 
 router.get('/login', (req, res) => {
 
@@ -14,46 +15,65 @@ router.get('/register', (req, res) => {
     res.render("users/register");
 })
 
-router.post('/add', (req, res) => {
+router.post('/register', (req, res) => {
     //do some serve side validation
+    let errors = [];
 
+    if (req.body.password != req.body.password2) {
+        const msg = "PASSWORD DONT MATCH";
+        errors.push({
+            msg
+        });
+    }
+    if (req.body.password.length < 6) {
+        const msg = "PASSWORD CANNOT BE LESS THAN 6 CHARACTERS";
+        errors.push({
+            msg
+        });
+    }
 
-    const new_user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    if (errors.length > 0) {
+        console.log(errors);
 
-    //hash the password
-    bcrypt.hash(new_user.password, 8, (err, hash) => {
-        if (err) {
-            console.log(err);
-        }
-        new_user.password = hash;
-        new_user.save()
-            .then(() => res.redirect("/users/login"))
-            .catch(err => console.log(err))
-    })
+        res.render("users/register", {
+            errors,
+            name: req.body.name,
+            email: req.body.email
+
+        })
+    } else {
+        const new_user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        //hash the password
+        bcrypt.hash(new_user.password, 8, (err, hash) => {
+            if (err) {
+                console.log(err);
+            }
+            new_user.password = hash;
+            new_user.save()
+                .then(() => res.redirect("/users/login"))
+                .catch(err => console.log(err))
+        })
+    }
 })
 
-router.post("/login/check", (req, res) => {
-    User.findOne({
-            email: req.body.email
-        })
-        .then(user => {
+router.post("/login", (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next)
 
+})
 
-            bcrypt.compare(req.body.password, user.password, (err, res) => {
-                if (err) {
-                    console.log(err);
-
-                }
-                console.log("password match");
-            })
-        })
-
-        .catch(err => console.log(err));
-
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('sucess_msg', "you are now logged out");
+    res.redirect('/users/login')
 })
 
 module.exports = router;
