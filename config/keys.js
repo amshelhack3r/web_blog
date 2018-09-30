1a00a929f1651474eee365d4f16c761056b7c028
amshelhack3r <amshelhack3r@gmail.com>
2018-09-30 20:07:45 +0300

    added a session secret

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mlab: "mongodb://amshel:xanthosis1@ds149672.mlab.com:49672/mutall_blog"
+    mlab: "mongodb://amshel:xanthosis1@ds149672.mlab.com:49672/mutall_blog",
+    secret: "niggahigga"
 }
