4a5b097de8f0f13f84bf386881f00dfa5c008551
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 11:48:13 +0300

    add root variable

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mlab: "mongodb://amshel:xanthosis1@ds149672.mlab.com:49672/mutall_blog",
-    secret: "niggahigga"
-}
+  mlab: "mongodb://amshel:xanthosis1@ds149672.mlab.com:49672/mutall_blog",
+  secret: "niggahigga",
+  rootpath: ""
+};
