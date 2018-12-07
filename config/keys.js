239c4ca6e0f993a34153b6fa8311d614ce70cea5
amshelhack3r <amshelhack3r@gmail.com>
2018-12-07 12:18:56 +0300

    change root

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mlab: "mongodb://amshel:xanthosis1@ds149672.mlab.com:49672/mutall_blog",
   secret: "niggahigga",
-  rootpath: "/node_projects"
+  rootpath: "/node_projects/blog"
 };
